6d4cb68cd6b2b946535537ea1dcebd1afbedd367
thibaut lenoir <lenoir677@gmail.com>
2022-04-05 20:24:11 +0200

    affichage page produit

--- back/controllers/product.js
+++ back/controllers/product.js
@@ -90,3 +90,7 @@ exports.orderProducts = (req, res, next) => {
     }
   );
 };
+
+
+
+
--- front/js/script.js
+++ front/js/script.js
@@ -1,6 +1,6 @@
 
-
-const getData = async () => {
+/*récupération des données du serveur et placement des données dans la page index*/ 
+const DataProducts = async () => {
     await fetch("http://localhost:3000/api/products")
     .then(reponse => reponse.json())
     .then(canapes => {
@@ -8,11 +8,12 @@ const getData = async () => {
         canapes.forEach(canap =>{
             html += buildHtml(canap)
         })
-        document.getElementById("items")
-        .innerHTML = html;
+    document.getElementById("items")
+    .innerHTML = html;
     });
 };
- getData();
+ DataProducts();
+
 function buildHtml(canap){
     return    `<a href="./product.html?id=42">
         <article>
@@ -21,6 +22,72 @@ function buildHtml(canap){
             <p class="productDescription">${canap.description}</p>
         </article>
     </a> `
-
 }
-   
+
+/*===============================================================*/
+window.addEventListener('items', () => {
+    let canape = JSON.parse(canapes);
+    let searchParams = new URLSearchParams(window.location.search);
+
+    if(searchParams.has('_id')){
+        let canapeId = searchParams.get('_id');
+        console.log(canapeId);
+    } else {
+        window.location.pathname = 'index.html'
+    }
+})
+/*===================================================================*/
+/* récupération produit et affichage sur page produit*/
+const url = new URL("http://localhost:3000/api/products");
+    
+const getData = async () => {
+    let response = await fetch(url)
+    if (response.ok){
+        let data = await response.json()
+        console.log(data)
+    };}
+getData()
+
+
+const writeHtml = async (data) => {
+    let img = document.createElement('img');
+    img.src = data.imageUrl;
+    img.innerHTML = data.img;
+    
+    let title = document.getElementById('title');
+    title.innerHTML = data.name;
+    
+    let price = document.getElementById('price');
+    price.innerHTML = data.price;
+    
+    let description = document.getElementById('description');
+    description.innerHTML = data.description;
+    
+    
+    let option = document.createElement('option');
+    option.value = data.colors;
+    option.innerHTML = data.colors;
+    
+    let colors = document.getElementById('colors');
+    colors.appendChild(option);
+    r
+}
+writeHtml(data)
+
+/*============================================================*/
+/* bouton commander*/
+    const clickButton = document.querySelector("#addToCart");
+
+    clickButton.addEventListener('click', () =>{
+        ajouteAuPanier
+    })
+
+    function ajouteAuPanier(){
+        for (let product = 0;; product++){
+            let totalProduct = product.length;
+            document.getDocumentById("totalQuantity")
+                totalProduct.innerHTML = totalProduct; 
+               
+        }
+    
+    }
