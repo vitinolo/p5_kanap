1a5d0b327a76d8d4b23b8ba844982678a1628803
thibaut lenoir <lenoir677@gmail.com>
2022-04-11 10:15:10 +0200

    affichage produit

--- front/js/script.js
+++ front/js/script.js
@@ -1,7 +1,7 @@
 
-/*récupération des données du serveur et placement des données dans la page index*/ 
+//récupération des données du serveur et placement des données dans la page index 
 const DataProducts = async () => {
-    await fetch("http://localhost:3000/api/products")
+    await fetch("http://localhost:3000/api/products/")
     .then(reponse => reponse.json())
     .then(canapes => {
         let html = "";
@@ -13,82 +13,15 @@ const DataProducts = async () => {
     });
 };
  DataProducts();
-
+//construction du html
 function buildHtml(canap){
-    return    `<a href="./product.html?id=42">
-        <article>
+    
+        return    `<a href="./product.html?id=${canap._id}">
+    <article>
             <img src="${canap.imageUrl}" alt="photos de différents canapés">
             <h3 class="productName">${canap.name}</h3>
             <p class="productDescription">${canap.description}</p>
         </article>
     </a> `
+    
 }
-
-/*===================================================================*/
-/* récupération des produits*/
-const url = new URL("http://localhost:3000/api/products");
-
-const getData = async () => {
-    let response = await fetch(url)
-    if (response.ok){
-        let data = await response.json()
-        console.log(data)
-    };}
-    getData()
-/*===============================================================*/
-/* écoute du produit cliqué*/
-window.addEventListener('items', () => {
-    let canape = JSON.parse(canapes);
-    let searchParams = new URLSearchParams(window.location.search);
-
-    if(searchParams.has('_id')){
-        let canapeId = searchParams.get('_id');
-        console.log(canapeId);
-    } else {
-        window.location.pathname = 'index.html'
-    }
-})
-
-/* construction de la page produit*/
-const writeHtml = async (data) => {
-    let img = document.createElement('img');
-    img.src = data.imageUrl;
-    img.innerHTML = data.img;
-    
-    let title = document.getElementById('title');
-    title.innerHTML = data.name;
-    
-    let price = document.getElementById('price');
-    price.innerHTML = data.price;
-    
-    let description = document.getElementById('description');
-    description.innerHTML = data.description;
-    
-    
-    let option = document.createElement('option');
-    option.value = data.colors;
-    option.innerHTML = data.colors;
-    
-    let colors = document.getElementById('colors');
-    colors.appendChild(option);
-    r
-}
-writeHtml(data)
-
-/*============================================================*/
-/* bouton commander*/
-    const clickButton = document.querySelector("#addToCart");
-
-    clickButton.addEventListener('click', () =>{
-        ajouteAuPanier
-    })
-
-    function ajouteAuPanier(){
-        for (let product = 0;; product++){
-            let totalProduct = product.length;
-            document.getDocumentById("totalQuantity")
-                totalProduct.innerHTML = totalProduct; 
-               
-        }
-    
-    }
