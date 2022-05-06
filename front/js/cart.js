bb6fcfeb94887afdde7530c31321b7c0c7212591
thibaut lenoir <lenoir677@gmail.com>
2022-05-06 20:09:15 +0200

    formulaire + suprime article

--- front/js/cart.js
+++ front/js/cart.js
@@ -14,9 +14,12 @@ if (!localStorage.getItem('products'))
 
         list.forEach(canap =>{ listenForQtyChange(canap) })
 
+        list.forEach(canap => { removeCanap(canap) });
+
         updateTotal(list);
 
         listenForCardSubmit();
+
     });  
 }
 
@@ -74,7 +77,6 @@ function listenForQtyChange(canap)
     input.addEventListener('change', (e) =>
     {
         let qty = e.target.value
-        console.log('on essaye de changer la qté');
 
         if( qty < 1){
             alert ("vous devez rentrer une valeur entre 1 et 100");
@@ -89,9 +91,99 @@ function listenForQtyChange(canap)
         const canapInStorage = products.find(item => item.id === canap._id && item.color === canap.color)
         canapInStorage.qty = qty;
 
-        console.log(canapInStorage);
         localStorage.setItem('products', JSON.stringify(products))
         location.reload();       
+    })
+}
+
+function listenForCardSubmit() 
+{   
+    const firstNameInput = document.getElementById('firstName');
+    firstNameInput.addEventListener('input', (e) =>
+    {
+        const firstName = e.target.value;
+       
+        if (firstName.length < 2 )
+        {
+           document.getElementById('firstNameErrorMsg').innerText = 'Merci de bien remplir le champ prénom';
+       } 
+       else
+       {
+        document.getElementById('firstNameErrorMsg').innerText = '';  
+       }
+    })
+
+    const lastNameInput = document.getElementById('lastName');
+    lastNameInput.addEventListener('input', (e) =>
+    {
+       const lastName = e.target.value;
+
+       if (lastName.length < 2 )
+       {
+           document.getElementById('lastNameErrorMsg').innerText = 'Merci de bien remplir le champ nom';
+        } 
+        else
+       {
+        document.getElementById('lastNameErrorMsg').innerText = '';  
+       }
+    })
+    
+    const addressInput = document.getElementById('address');
+    addressInput.addEventListener('input', (e) =>
+    {
+        const address = e.target.value;
+       
+        if (address.length < 2 )
+        {
+            document.getElementById('addressErrorMsg').innerText = 'Merci de bien remplir le champ adresse';
+        } 
+        else
+       {
+        document.getElementById('addressErrorMsg').innerText = '';  
+    }
+})
+
+    const cityInput = document.getElementById('city');
+    cityInput.addEventListener('input', (e) =>
+    {
+       const city = e.target.value;
+       
+       if (city.length < 2 )
+       {
+           document.getElementById('cityErrorMsg').innerText = 'Merci de bien remplir le champ ville';
+        } 
+        else
+       {
+        document.getElementById('cityErrorMsg').innerText = '';  
+    }
+    })
+
+    const emailInput = document.getElementById('email');
+    emailInput.addEventListener('input', (e) =>
+    {
+       const email = e.target.value;
+       
+       if (email.length < 2 )
+       {
+           document.getElementById('emailErrorMsg').innerText = 'Merci de bien remplir le champ email';
+       } 
+       else
+       {
+        document.getElementById('emailErrorMsg').innerText = '';  
+       }
+    })
+}
+
+function removeCanap(canap)
+{   
+    let removeButton = document.querySelector(`article[data-id="${canap._id}-${canap.color}"] .deleteItem`);
+    removeButton.addEventListener('click', () =>{
+       let products = JSON.parse(localStorage.getItem('products'));
+       let canapOnClickRemoved = document.querySelector(`article[data-id="${canap._id}-${canap.color}"]`);
+       canapOnClickRemoved.remove();
+       products = products - canapOnClickRemoved
+       JSON.parse(localStorage.getItem('products', products));
+       console.log("un en moins !")             
     })
 }
 
@@ -100,28 +192,12 @@ function updateTotal(canapes)
     let qty = 0;
     let total = 0;
 
-    canapes.forEach(canap =>{
+    canapes.forEach(canap =>
+        {
         qty = qty + Number(canap.qty);
         total = total + (Number(canap.price) * canap.qty);
-    })
+        })
+
     document.getElementById('totalQuantity').innerHTML = qty
     document.getElementById('totalPrice').innerHTML = format(total)
-}
-
-function listenForCardSubmit() 
-{
-    const firstNameInput = document.getElementById('firstName');
-    firstNameInput.addEventListener('input', (e) =>
-    {
-       const firstName = e.target.value;
-       
-       if (firstName.length < 2 )
-       {
-           document.getElementById('firstNameErrorMsg').innerText = 'Merci de bien remplir le champ prénom';
-       } 
-       else
-       {
-        document.getElementById('firstNameErrorMsg').innerText = '';  
-       }
-    })
 }
